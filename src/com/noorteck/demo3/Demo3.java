21984d0a1fe4ba37017f812fa52f343bcfcb5531
12023 <12023@LAPTOP-SF6VHV5D.mynetworksettings.com>
2023-01-10 21:48:33 -0500

    2nd edit

--- src/com/noorteck/demo3/Demo3.java
+++ src/com/noorteck/demo3/Demo3.java
@@ -1,5 +1,12 @@
 package com.noorteck.demo3;
 
 public class Demo3 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Github");
+		
+		System.out.println("2nd edit");
+	}
 
 }
